c478d3c495c97ac152c7c03d98cd3701d555e1dc
maokangkun <maokangkun@pjlab.org.cn>
2026-09-14 16:08:20 +0800

    ship prebuilt binaries instead of building from source

--- Formula/csusage.rb
+++ Formula/csusage.rb
@@ -1,14 +1,31 @@
 class Csusage < Formula
   desc "Token usage reports for Claude Science"
   homepage "https://github.com/maokangkun/csusage"
-  url "https://static.crates.io/crates/csusage/csusage-0.1.1.crate"
-  sha256 "4d625bc4c5f6813c06c7da6ee08dc0d2857343f6e20d3dbc7f75d097177ab534"
+  version "0.1.1"
   license "MIT"
 
-  depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/maokangkun/csusage/releases/download/v0.1.1/csusage-v0.1.1-x86_64-apple-darwin.tar.gz"
+      sha256 "d6d67808a0f6db07b463a6c79763503e6724928c8a393d310736d5418d3ab10a"
+    else
+      url "https://github.com/maokangkun/csusage/releases/download/v0.1.1/csusage-v0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "6401aec0df33b2c3cf713aed8b0a4559397ab44a62dc3fc23cd2ded6acb74765"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/maokangkun/csusage/releases/download/v0.1.1/csusage-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "961efa3eca6a879d907d92a47815bc9ff1406c6e89b215a21a436f67bd0f6b59"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/maokangkun/csusage/releases/download/v0.1.1/csusage-v0.1.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "1b78c3edf042c2d2f2f1f783d9c36c0a73f0ae987f93e9557ecb766c656e1815"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "csusage"
   end
 
   test do
